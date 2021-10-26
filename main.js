81dc02b2f2188251fe09d154a69a081eeb95b08b
wonaval <wonaval@gmail.com>
2021-10-26 16:57:23 -0400

    Added function that places 10 random squares on the grid

--- main.js
+++ main.js
@@ -8,12 +8,43 @@ console.log("js loaded");
 
 
 // VIEW
+const gridEls = Array.from(document.querySelectorAll('.grid > div > div'));
+const playerBoard = new Array(100).fill(0);
+const cpuBoard =  new Array(100).fill(0);
 
+gridEls.forEach((square)=> {
+    square.addEventListener('click', (evt) => test(event))
+});
 
+function test (evt) {
+    console.log(evt.target)
+}
 
-
-
-
-
-
+random(10);
 // CONTROLLER
+function random (amount) {
+    while (amount > 0) {
+    let random = Math.floor(Math.random() * 100);
+    console.log(random);
+    if (playerBoard[random] === 0) {
+        playerBoard[random] = 1
+        amount--;
+    }
+    }
+}
+
+
+
+console.log(gridEls, playerBoard, cpuBoard);
+
+render();
+
+function render () {
+    playerBoard.forEach((square, idx) => {
+        console.log(square, idx)
+        if (square === 1) {
+            console.log(gridEls[idx])
+            gridEls[idx].style.background = "#FFFFFF"
+        }
+    })
+}
