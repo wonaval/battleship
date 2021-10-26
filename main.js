c88bd2bc04329b1a9501747f0fba180e3be5372e
wonaval <wonaval@gmail.com>
2021-10-26 19:56:04 -0400

    Able to click on squares and confirm hit/miss

--- main.js
+++ main.js
@@ -33,13 +33,33 @@ const cpuBoard = [
 
 console.log(gridEls, playerBoard, cpuBoard);
 
-gridEls.forEach((square)=> {
+gridEls.forEach((square) => {
     square.addEventListener('click', (evt) => squareHandle(evt))
 });
 
 function squareHandle (evt) {
-    console.log(evt.target)
-    
+    let eventID = evt.target.getAttribute("id");
+    console.log(eventID, typeof(eventID));
+    let element = parseInt(eventID.slice(1, 3));
+    let x = eventID.charAt(1);
+    let y = eventID.charAt(2);
+    let squareValue = playerBoard[y][x]
+    console.log(x, y, squareValue, element)
+
+    if (squareValue <= 1) {
+        if (squareValue === 1) {
+            gridEls[element].style.backgroundColor = "red";
+            gridEls[element].innerText = "X";
+            playerBoard[y][x] += 2;
+        } else {
+            gridEls[element].style.backgroundColor = "white";
+            gridEls[element].innerText = "O";
+            playerBoard[y][x] += 2;
+        }
+    } else {
+        console.log(`Square ${eventID} already selected!`)
+    }
+    console.log(playerBoard);
 }
 
 // CONTROLLER
@@ -50,16 +70,16 @@ function random (amount) {
     while (amount > 0) {
         let x = Math.floor(Math.random() * 10);
         let y = Math.floor(Math.random() * 10);
-        console.log(x, y);
-        console.log(playerBoard[x][y]);
+        // console.log(x, y);
+        // console.log(playerBoard[x][y]);
         if (playerBoard[x][y] === 0) {
             playerBoard[x][y] = 1;
             amount--;
         } else {
             console.log("Square already placed!")
         }
-        console.log(playerBoard)
     }
+    console.log(playerBoard)
 }
 
 
@@ -67,14 +87,17 @@ function random (amount) {
 render();
 
 function render () {
+    let test = 0;
     playerBoard.forEach((row, idx) => {
-        console.log(playerBoard[idx]);
+        // console.log(playerBoard[idx]);
         playerBoard[idx].forEach((col, idy) => {
             if (playerBoard[idx][idy] === 1) {
-                let index = `${(idx*10) + idy}`;
-                console.log(index)
-                console.log(gridEls[index])
-                gridEls[index].style.backgroundColor = "#737373";
+                let index = `${(idy*10) + idx}`;
+                // console.log(index);
+                // console.log(gridEls[index]);
+                // gridEls[index].style.backgroundColor = "#737373";
+                // gridEls[index].innerText = test;
+                test++;
             }
         });
     });
